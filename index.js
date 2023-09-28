a442a118df9f1deacb3d66c905606ba618f6b496
balavane <danielbashonga88@gmail.com>
2023-09-28 14:29:32 +0200

    Version 000

---
+++ index.js
@@ -0,0 +1,5 @@
+/* Bienve tache : 
+    - Creer trois 3 tableaux chacun de tableaux dois avoir 5 index dont chacune seras une chaine
+    qui aura un message.
+    Balavane tache:
+    - creer une fonction qui afficheras le message*/
