9bfeec8c41bc86b73c8d430b2bc858305972cd1d
balavane <danielbashonga88@gmail.com>
2023-09-28 14:46:34 +0200

    verssion 001

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 /* Bienve tache : 
     - Creer trois 3 tableaux chacun de tableaux dois avoir 5 index dont chacune seras une chaine
     qui aura un message.
+    - creer un Redmi
     Balavane tache:
     - creer une fonction qui afficheras le message*/
+    
