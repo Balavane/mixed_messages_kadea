6d8b40d78d03f117053dc30d6ff838d7d8362159
balavane <danielbashonga88@gmail.com>
2023-09-29 16:16:34 +0200

    version 002

--- index.js
+++ index.js
@@ -4,4 +4,25 @@
     - creer un Redmi
     Balavane tache:
     - creer une fonction qui afficheras le message*/
-    
+
+  // creation des mots aleatoire 
+ const fruits_aleatoire = Math.floor(Math.random()* fruit.length)
+ const object_aleatoire = Math.floor(Math.random()* object.length)
+ const number_aleatoire = Math.floor(Math.random()* number.length)
+  
+// Creation du fonction des mots aleatoires
+ const hasard_mots = () => {
+  let a = Math.floor(Math.random()* 3)
+  if(a == 0){
+    return fruit[fruits_aleatoire] + " : l'element du tableau fruit à l'index " + fruits_aleatoire
+  }else if (a == 1){
+   return object[object_aleatoire] + " : l'element du tableau object à l'index " + object_aleatoire 
+  }else{
+   return number[number_aleatoire] + " : l'element du tableau number à l'index "+ number_aleatoire
+  }
+}
+
+// affichage de mots aleatoire
+console.log(hasard_mots())
+  
+   
