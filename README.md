1b9d1c317af25e9092e3881de1ef8471da4537c1
bienvenu07 <kipsayclopia@gmail.com>
2023-10-02 16:08:30 +0200

    version02

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 (.) Creation des tableaux et dont chaque tableau a 5 index,
 
+
 (.) lorsque on passe a l'operation d'impression  pour appeler les elements des nos tableaux l'impression se  passe de facon aleatoire des index du tableau,
 
 
--- index.js
+++ index.js
@@ -4,16 +4,29 @@
     - creer un Redmi
     Balavane tache:
     - creer une fonction qui afficheras le message*/
-
-
+    let fruit = ["avocar","ananas","orange","citron","banane"];
+  
+    let object = ["latte","stylos","ordinateur","modem","agenda"];
+  
+    let number = [ "one","five","two","nani","six"];
   // creation des mots aleatoire 
-
-  let fruit = ["avocar","ananas","orange","citron","banane"];
+ const fruits_aleatoire = Math.floor(Math.random()* fruit.length)
+ const object_aleatoire = Math.floor(Math.random()* object.length)
+ const number_aleatoire = Math.floor(Math.random()* number.length)
   
-  let object = ["latte","stylos","ordinateur","modem","agenda"];
+// Creation du fonction des mots aleatoires
+ const hasard_mots = () => {
+  let a = Math.floor(Math.random()* 3)
+  if(a == 0){
+    return fruit[fruits_aleatoire] + " : l'element du tableau fruit à l'index " + fruits_aleatoire
+  }else if (a == 1){
+   return object[object_aleatoire] + " : l'element du tableau object à l'index " + object_aleatoire 
+  }else{
+   return number[number_aleatoire] + " : l'element du tableau number à l'index "+ number_aleatoire
+  }
+}
 
-  let number = [ "one","five","two","nani","six"];
- 
-  
+// affichage de mots aleatoire
+console.log(hasard_mots())
   
    
